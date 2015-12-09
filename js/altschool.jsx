24878d8bffe7d45c41defc47a90d73a31274dfa4
Andrew Bacon <abacon@gmail.com>
2015-12-09 13:30:12 -0800

    Render to mountpoint, use history.

--- js/altschool.jsx
+++ js/altschool.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link } from 'react-router'
+import { createHistory, useBasename } from 'history'
 
 import ListOfBoards from './routes/ListOfBoards'
 import EditBoard from './routes/EditBoard'
 
+var history = useBasename(createHistory)({
+  basename: '/'
+})
+
 var App = React.createClass({
   displayName: 'app',
   render: function() {
-    <span> This is my favorite app. </span>
+    return (<div>
+      <Link to='/boards'>Boards</Link>
+      <Link to='/'>Home</Link>
+    </div>)
   }
 })
 
-React.createClass({
-  render: function() {
-    return (
-      <Router>
-        <Route path="/" component={App}>
-          <Route path="boards" component={ListOfBoards} />
-          <Route path="board/:id" component={EditBoard} />
-        </Route>
-      </Router>
-    )
-  }
-})
+render((
+  <Router history={history} >
+    <Route path="/" component={App}>
+      <Route path="boards" component={ListOfBoards} />
+      <Route path="board/:id" component={EditBoard} />
+    </Route>
+  </Router>
+), document.getElementById('colorboard'))
