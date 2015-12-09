6a5ee34c3a64e36236f3a9efa8e7c6b923fc06d5
Andrew Bacon <abacon@gmail.com>
2015-12-09 14:58:37 -0800

    Add webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: [
+      './js/altschool.jsx'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+      },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?stage=1'},
+            { test: /\.less$/, loader: 'style!css!less!autoprefixer' },
+            { test: /\.css$/, loader: 'style!css!autoprefixer' },
+            { test: /\.json$/, loader: 'json'},
+          ]
+      },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+      extensions: ['', '.jsx', '.js', '.json', '.less']
+    }
+
+  }
