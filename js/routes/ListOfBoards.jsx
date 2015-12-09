5033bc353e30e8ef88b192bc264b6f2d5ec64149
Andrew Bacon <abacon@gmail.com>
2015-12-09 14:58:21 -0800

    Remove console.log

--- js/routes/ListOfBoards.jsx
+++ js/routes/ListOfBoards.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 var ListOfBoards = React.createClass({
   displayName: 'List of Boards',
   render: function() {
-    console.log('rendering boards.')
     return <span>A list of boards.</span>
   }
 })
